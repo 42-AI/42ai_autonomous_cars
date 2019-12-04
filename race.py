8a1dd33a2ff098f2b1d790b432254062ab622794
Pablo CLEMENT <pclement@e3r2p6.42.fr>
2019-12-04 18:48:41 +0100

    Refactor of race.py: use of classes with init loading model and modular functions and argparse

---
+++ race.py
@@ -0,0 +1,104 @@
+import numpy as np
+import time
+
+import argparse
+import Adafruit_PCA9685
+from keras.models import load_model
+
+from pivideostream import PiVideoStream
+from const import *
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model_path", type=str,
+                        help="Provide the model path.\n")
+    return parser.parse_args()
+
+
+class RaceOn:
+    def __init__(self, model_path):
+        # Load model
+        self.model = load_model(model_path)
+
+        # Init engines
+        # TODO: A quoi sert direction speed? ajouterhead pour initialiser
+        self.speed = SPEED_FAST
+        self.direction = DIR_C
+        self.head = H_DOWN
+        self.pwm = Adafruit_PCA9685.PCA9685()
+        self.pwm.set_pwm_freq(50)
+
+        # Create a *threaded *video stream, allow the camera sensor to warm_up
+        self.video_stream = PiVideoStream().start()
+        time.sleep(2)
+        self.video_stream.test()
+        self.frame = self.video_stream.read()
+
+    @staticmethod
+    def choose_direction(predictions):
+        # TODO: Make it more modular so  it can handle 3 or 5 direction--> dictionnary or list in utils.const?
+        if predictions[1] == 0:
+            return DIR_L
+        elif predictions[1] == 1:
+            return DIR_C
+        elif predictions[1] == 2:
+            return DIR_R
+
+    @staticmethod
+    def choose_speed(predictions):
+        if predictions[1] == 1 and predictions[0] == 1:
+            return SPEED_FAST
+        return SPEED_NORMAL
+
+    @staticmethod
+    def choose_head(predictions, speed):
+        if speed == SPEED_FAST and predictions[1] == 1 and predictions[0] == 1:
+            return H_UP
+        return H_DOWN
+
+    def race(self):
+        while True:
+            # Grab the self.frame from the threaded video stream
+            self.frame = self.video_stream.read()
+            image = np.array([self.frame]) / 255.0
+
+            # Get model prediction
+            predictions_raw = self.model.predict(image)
+            predictions = [np.argmax(pred, axis=1) for pred in predictions_raw]  # Why ?
+
+            # Decide action
+            direction = self.choose_direction(predictions)
+            speed = self.choose_speed(predictions)
+            head = self.choose_head(predictions, speed)
+
+            # Apply values to engines
+            self.pwm.set_pwm(0, 0, direction)
+            self.pwm.set_pwm(1, 0, speed)
+            self.pwm.set_pwm(2, 0, head)
+
+    # TODO reinitialize to init values
+    def stop(self):
+        self.pwm.set_pwm(0, 0, 0)
+        self.pwm.set_pwm(1, 0, 0)
+        self.pwm.set_pwm(2, 0, 0)
+        self.video_stream.stop()
+        print("Stop")
+
+
+if __name__ == '__main__':
+    options = get_args()
+    race_on = RaceOn(options.model_path)
+    print("Ready ! press CTRL+C to START/STOP :")
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        pass
+
+    try:
+        race_on.race()
+    except KeyboardInterrupt:
+        pass
+    race_on.stop()
