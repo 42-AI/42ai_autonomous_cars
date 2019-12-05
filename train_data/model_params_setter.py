223e5280180bca58504fcf722ae89f0d4220ae4b
Pablo CLEMENT <pclement@e1r3p4.42.fr>
2019-12-05 18:43:58 +0100

    Refactor train file: Made a class with modular functions, create another file that is the param_setter

---
+++ train_data/model_params_setter.py
@@ -0,0 +1,54 @@
+#  Patate/Data_processing/Training/3_directions_models_Opti_speed.ipynb
+
+import keras.backend as K
+from keras.layers import *
+
+
+def get_params():
+    # TODO: play with models this is Model from PatateV2
+
+    K.clear_session()
+    img_in = Input(shape=(96, 160, 3), name='img_in')
+    x = img_in
+
+    x = Convolution2D(2, (5, 5), strides=(2, 2), use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    x = Convolution2D(4, (5, 5), strides=(2, 2), use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    x = Dropout(.4)(x)
+    x = Convolution2D(8, (5, 5), strides=(2, 2), use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    x = Dropout(.5)(x)
+
+    x = Flatten(name='flattened')(x)
+
+    x = Dense(100, use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    x = Dropout(.4)(x)
+    x = Dense(50, use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    x = Dropout(.3)(x)
+
+    out_dir = Dense(3, activation='softmax')(x)
+    out_speed = Dense(2, activation='softmax')(x)
+
+    model_params = {
+        'model_name': "model_race_speed.h5",
+        'model_inputs': [img_in],
+        'model_outputs': [out_speed, out_dir],
+    }
+
+    dataset = "../../../../speed"
+    images_directory = dataset + '/Train'
+
+    return images_directory, model_params
+
+
+if __name__ == '__main__':
+    params = get_params()
+    print(params)
--- train_data/train.py
+++ train_data/train.py
@@ -1,3 +1,5 @@
+#  Patate/Data_processing/Training/3_directions_models_Opti_speed.ipynb
+
 import os
 import random
 import numpy as np
@@ -5,12 +7,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import keras.backend as K
 import keras.callbacks
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
-from keras.models import Model, Sequential
+from keras.models import Model
 from keras.layers import *
+
+import model_params_setter
 
 
 class TrainModel:
@@ -19,14 +22,11 @@ class TrainModel:
         self.model_name = model_params['name']
         self.model_inputs = model_params['inputs']
         self.model_outputs = model_params['outputs']
-        
-    def load_images(self):
-        images = []
-        labels_speed = []
-        labels_dir = []
-        dir_list = os.listdir(self.images_dir)
-        random.shuffle(dir_list)
-        for filename in dir_list:
+        self.model = None
+
+    def load_images(self, show_test=True):
+        images, labels_speed, labels_directions = [], [], []
+        for filename in random.shuffle(os.listdir(self.images_dir)):
             filepath = self.images_dir + '/' + filename
             # load an image from file
             image = load_img(filepath, target_size=(96, 160))
@@ -35,93 +35,40 @@ class TrainModel:
             # get image id + labels
             value_dir = float(filename.split('_')[1])
             value_speed = float(filename.split('_')[0])
-            labels_dir.append(value_dir)
+            labels_directions.append(value_dir)
             labels_speed.append(value_speed)
             images.append(image)
-        return images, labels_speed, labels_dir
+            labels_directions = np.array(pd.get_dummies(labels_directions))
+            labels_speed = np.array(pd.get_dummies(labels_speed))
+        images = self.normalize_images(images)
+        if show_test:
+            print('Loaded Images and labels for training: {}'.format(len(images)))
+            print("For the image below, direction label is: {} and speed label is {}".format(labels_directions[42],
+                                                                                             labels_speed[42]))
+            plt.imshow(images[42])
+        return images, labels_speed, labels_directions
 
     @staticmethod
     def normalize_images(images):
-        return images / 255.0
+        return np.array(images) / 255.0
+
+    def train(self):
+        images, labels_speed, labels_directions = self.load_images()
+        self.model = Model(inputs=self.model_inputs, outputs=self.model_outputs)
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        print(self.model.summary)
+        best_checkpoint = keras.callbacks.ModelCheckpoint(self.model_name, monitor='val_loss', verbose=1,
+                                                          save_best_only=True, mode='min')
+        self.model.fit(images, [labels_speed, labels_directions], batch_size=64, epochs=100, validation_split=0.2,
+                       verbose=1, callbacks=[best_checkpoint])
+
+    def graph(self):
+        history_df = pd.DataFrame(self.model.history, index=self.model.epoch)
+        history_df.plot(ylim=(0, 1))
 
 
 if __name__ == '__main__':
-    dataset = "../../../../speed"
-    # load images from both train and test groups
-    images_directory = dataset+'/Train'
-    train_model = TrainModel(images_directory)
-    images, labels_speed, labels_dir = load_images(images_directory)
-    print('Loaded Images and labels for training: {}'.format(len(images)))
-    images =
-
-#Normalise images
-images = np.array(images)
-images /= 255.0
-
-#convert datas to dummyvalues
-labels_dir = np.array(pd.get_dummies(labels_dir))
-labels_speed = np.array(pd.get_dummies(labels_speed))
-
-labels_dir[42], labels_speed[42], plt.imshow(images[42])
-
-
-
-
-'''
-Model from PatateV2
-'''
-
-K.clear_session()
-#############################################################
-
-img_in = Input(shape=(96, 160, 3), name='img_in')
-x = img_in
-
-x = Convolution2D(2, (5,5), strides=(2,2), use_bias=False)(x)
-x = BatchNormalization()(x)
-x = Activation("relu")(x)
-x = Convolution2D(4, (5,5), strides=(2,2), use_bias=False)(x)
-x = BatchNormalization()(x)
-x = Activation("relu")(x)
-x = Dropout(.4)(x)
-x = Convolution2D(8, (5,5), strides=(2,2), use_bias=False)(x)
-x = BatchNormalization()(x)
-x = Activation("relu")(x)
-x = Dropout(.5)(x)
-
-x = Flatten(name='flattened')(x)
-
-x = Dense(100, use_bias=False)(x)
-x = BatchNormalization()(x)
-x = Activation("relu")(x)
-x = Dropout(.4)(x)
-x = Dense(50, use_bias=False)(x)
-x = BatchNormalization()(x)
-x = Activation("relu")(x)
-x = Dropout(.3)(x)
-
-out_dir = Dense(3, activation='softmax')(x)
-out_speed = Dense(2, activation='softmax')(x)
-
-
-# Compile Model
-model = Model(inputs=[img_in], outputs=[out_speed, out_dir])
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-model.summary()
-
-
-model_name="model_race_speed.h5"
-
-#save best model if model improved
-best_checkpoint = keras.callbacks.ModelCheckpoint(model_name, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-
-
-h = model.fit(images, [labels_speed, labels_dir], batch_size=64, epochs=100, validation_split=0.2, verbose=1, callbacks=[best_checkpoint])
-
-
-#print History graph
-historydf = pd.DataFrame(h.history, index=h.epoch)
-historydf.plot(ylim=(0, 1))
-
-
+    images_directory, model_parameters = model_params_setter.get_params()
+    train_model = TrainModel(images_directory, model_parameters)
+    train_model.train()
+    train_model.graph()
