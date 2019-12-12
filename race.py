32b8962f7a46f5e33fd8dd976101c5959c1d503f
jjauzion <jjauzion@student.42.fr>
2019-12-12 17:17:42 +0100

    wip: creating race training fct
    wip: race_trial() v1; but trial should be moved to race() and if cond to avoid code repeat

--- race.py
+++ race.py
@@ -1,12 +1,15 @@
 import argparse
 import numpy as np
 import time
+from PIL import Image
+from collections import deque
+from pathlib import Path
 
 import Adafruit_PCA9685
 # noinspection PyUnresolvedReferences
 from keras.models import load_model
 
-from pivideostream import PiVideoStream
+from utils.pivideostream import PiVideoStream
 from utils.const import SPEED_NORMAL, SPEED_FAST, HEAD_UP, HEAD_DOWN, \
     DIRECTION_R, DIRECTION_L, DIRECTION_C, DIRECTION_L_M, DIRECTION_R_M
 
@@ -15,6 +18,8 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("model_path", type=str,
                         help="Provide the model path.\n")
+    parser.add_argument("-t", "--trial", action="store_true",
+                        help="Run in trail mode: record las 'buff_size' picture and measure pred rate.\n")
     return parser.parse_args()
 
 
@@ -39,6 +44,9 @@ class RaceOn:
         time.sleep(2)
         self.video_stream.test()
         self.frame = self.video_stream.read()
+        self.buffer = None
+        self.nb_pred = 0
+        self.start_time = 0
         print("RaceOn initialized")
 
     @staticmethod
@@ -67,13 +75,39 @@ class RaceOn:
             return HEAD_UP
         return HEAD_DOWN
 
+    def race_trial(self, buff_size=100):
+        self.buffer = deque(maxlen=buff_size)
+        self.start_time = time.time()
+        while True:
+            # Grab the self.frame from the threaded video stream
+            self.frame = self.video_stream.read()
+            sample = {"array": self.frame}
+            image = np.array([self.frame]) / 255.0  # [jj] Do we need to create a new array ? img = self.frame / 255. ?
+
+            # Get model prediction
+            predictions_raw = self.model.predict(image)
+            predictions = [np.argmax(pred, axis=1) for pred in predictions_raw]  # Why ?
+
+            # Decide action
+            direction = self.choose_direction(predictions)
+            speed = self.choose_speed(predictions)
+            head = self.choose_head(predictions, speed)
+            sample["direction"] = predictions[1]
+            sample["speed"] = 1 if speed == SPEED_FAST else 0
+            self.buffer.append(sample)
+
+            # Apply values to engines
+            self.pwm.set_pwm(0, 0, direction)
+            self.pwm.set_pwm(1, 0, speed)
+            self.nb_pred += 1
+
     def race(self, show_pred=False):
         speed = SPEED_NORMAL
         self.racing = True
         while self.racing:
-            # Grab the self.frame from the threaded video stream
+        # Grab the self.frame from the threaded video stream
             self.frame = self.video_stream.read()
-            image = np.array([self.frame]) / 255.0
+            image = np.array([self.frame]) / 255.0  # [jj] Do we need to create a new array ? img = self.frame / 255. ?
 
             # Get model prediction
             predictions_raw = self.model.predict(image)
@@ -92,6 +126,7 @@ class RaceOn:
             self.pwm.set_pwm(0, 0, direction)
             self.pwm.set_pwm(1, 0, speed)
             self.pwm.set_pwm(2, 0, head)
+            self.nb_pred += 1
 
     # TODO (pclement) reinitialize to init values
     def stop(self):
@@ -100,6 +135,21 @@ class RaceOn:
         self.pwm.set_pwm(1, 0, 0)
         self.pwm.set_pwm(2, 0, 0)
         self.video_stream.stop()
+
+        # Performance status
+        elapse_time = time.time() - self.start_time
+        pred_rate = self.nb_pred / float(elapse_time)
+        print('{} prediction in {}s -> {} pred/s'.format(self.nb_pred, elapse_time, pred_rate))
+
+        # Write buffer
+        if self.buffer is not None and len(self.buffer) > 0:
+            output_folder = Path("output")
+            print('Saving buffer pictures to : "{}"'.format(output_folder))
+            for i, img in enumerate(self.buffer):
+                pic_file = Path('{}_{}_image{}.jpg'.format(img["speed"], img["direction"], i))
+                pic = Image.fromarray(img["array"], 'RGB')
+                pic.save(output_folder / pic_file)
+            print('{} pictures saved.'.format(i + 1))
         print("Stop")
 
 
@@ -131,7 +181,8 @@ if __name__ == '__main__':
                 started = True
             elif user_input == "show" and not started:
                 print("Race is on. test mode")
-                race_on.race(show_pred=True)
+                race_on.race_trial()
+                # race_on.race(show_pred=True)
                 started = True
             elif user_input == "q":
                 keep_going = False
