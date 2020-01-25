72f0602eb38c518405bc8e9d641f84af2f1ec687
jjauzion <wants-to-fly@hotmail.fr>
2020-01-25 17:11:18 +0100

    test stuff...

--- get_data/search_picture.py
+++ get_data/search_picture.py
@@ -1,2 +1,28 @@
 import elasticsearch_dsl as esdsl
 
+from get_data.utils import es_utils
+from get_data.cluster_param import ES_HOST_IP, ES_INDEX, ES_HOST_PORT
+
+
+def create_search(es, index):
+    return esdsl.Search(using=es, index=index)
+
+
+def add_search_query(search_obj, s_type, query):
+    search_obj.query(s_type, **query)
+
+
+def get_search_param(dictionary):
+    es = es_utils.get_es_session(host_ip=ES_HOST_IP, port=ES_HOST_PORT)
+    s = esdsl.Search(using=es, index="patate-db").query("match", **{"event": "test"})
+    s = s.query("range", **{"time": {"gt": 10}})
+    s.query("match", **{"timestamp": "10/20/2020"})
+    print(s.to_dict())
+    exit()
+    for key in dictionary:
+        if "type" in dictionary[key]:
+            add_search_query(s, dictionary[key])
+            return
+        search_field = key
+        search_param = dictionary[key]
+
---
+++ test_search.py
@@ -0,0 +1,5 @@
+from get_data import search_picture
+
+
+if __name__ == "__main__":
+    search_picture.get_search_param({})
