fef6ecc608aa9202b0284f56642a63bd99dd4ba2
jjauzion <jjauzion@student.42.fr>
2020-01-29 13:51:49 +0100

    ongoing

--- conf/mappings.py
+++
@@ -1,2 +0,0 @@
-def five_direction():
-    return [250, 290, 327, 364, 400]
--- utils/car_mapping.py
+++ utils/car_mapping.py
@@ -1,6 +1,73 @@
 from conf.const import SPEED_NORMAL, SPEED_FAST, DIRECTION_L_M, DIRECTION_C, DIRECTION_L, DIRECTION_R, DIRECTION_R_M
 from conf.const import MAX_DIRECTION_LEFT, MAX_DIRECTION_RIGHT, MAX_SPEED, STOP_SPEED
-from conf.const import RAW_DIR_TO_LABEL_MAPPING
+from conf.const import RAW_DIR_TO_LABEL_MAPPING, JOYSTICK_TO_RAW_DIR_MAPPING, LABEL_TO_RAW_DIR_MAPPING
+from conf.const import TRIGGER_TO_RAW_SPEED_MAPPING, RAW_SPEED_TO_LABEL_MAPPING, LABEL_TO_RAW_SPEED_MAPPING
+
+
+class CarMapping:
+
+    @staticmethod
+    def get_linear_coef(p1, p2):
+        """
+        Compute the slope and intercept of the linear function passing through point p1 and point p2.
+        y1 = a * x1 + b
+        y2 = a * x2 + b
+        -> (y1 - y2) = a (x1 - x2) -> a = (y1 - y2) / (x1 - x2)
+        -> b = y1 - a * x1
+        :param p1:      [tuple]     point defined as a tuple: (x, y). 'x' and 'y' can be float or int
+        :param p2:      [tuple]     point defined as a tuple: (x, y). 'x' and 'y' can be float or int
+        :return:        [tuple]     (slope, intercept)
+        """
+        slope = (p1[1] - p2[1]) / (p1[0] - p2[0])
+        intercept = p1[1] - slope * p1[0]
+        return slope, intercept
+
+    def __init__(self):
+        self.joystick_to_raw_dir_mapping = JOYSTICK_TO_RAW_DIR_MAPPING
+        self.raw_dir_to_label_mapping = RAW_DIR_TO_LABEL_MAPPING
+        self.label_to_raw_dir_mapping = LABEL_TO_RAW_DIR_MAPPING
+        self.trigger_to_raw_speed_mapping = TRIGGER_TO_RAW_SPEED_MAPPING
+        self.raw_speed_to_label = RAW_SPEED_TO_LABEL_MAPPING
+        self.label_to_raw_speed_mapping = LABEL_TO_RAW_SPEED_MAPPING
+        if len(self.joystick_to_raw_dir_mapping) == 0:
+            self.linear = True
+            self.slope, self.intercept = self.get_linear_coef((-1, MAX_DIRECTION_LEFT), (1, MAX_DIRECTION_RIGHT))
+        else:
+            self.linear = False
+            self.slope = self.intercept = None
+
+    def get_label_from_raw_dir(self, direction):
+        """
+        Return the label value from the raw direction value according to the defined mapping.
+        :param direction:       [int or float]  direction value (float accepted for linear mapping only)
+        :return:                [int]           label value
+        """
+        if len(self.raw_dir_to_label_mapping) == 0:
+            raise ValueError("Linear label mapping not yet implemented")
+        try:
+            return self.raw_dir_to_label_mapping.index(direction)
+        except ValueError as err:
+            err.args = (f'"{direction}" is not in the direction to label mapping : {self.raw_dir_to_label_mapping}',)
+            raise
+
+    def get_speed_from_xbox_trigger(trigger):
+        return _linear_speed_trigger_mapping(trigger)
+
+    def get_raw_dir_from_xbox_joystick(self, joystick):
+        """
+        Return the raw direction value from the xbox joystick position according to the defined mapping.
+        :param joystick:        [float]         joystick position
+        :return:                [int]           raw direction value
+        """
+        if self.linear:
+            return self.slope * joystick + self.intercept
+        for i, position in self.joystick_to_raw_dir_mapping:
+            if joystick < position:
+                return self.label_to_raw_dir_mapping[i]
+        return self.label_to_raw_dir_mapping[-1]
+
+    def get_label_from_speed(speed):
+        return _two_label_speed_mapping(speed)
 
 
 def _two_label_speed_mapping(speed):
@@ -33,22 +100,6 @@ def _five_label_direction_mapping(direction):
         return 3
     elif direction == DIRECTION_R_M:
         return 4
-
-
-def _label_direction_mapping(direction):
-    """
-    Return a label value from the direction value as defined by the list RAW_DIR_TO_LABEL_MAPPING. If this list is empty
-    , then linear mapping is applied.
-    :param direction:       [int or float]   direction value (float accepted for linear mapping only)
-    :return:
-    """
-    if len(direction) == 0:
-        raise ValueError("Linear label mapping noy yet implemented")
-    try:
-        return RAW_DIR_TO_LABEL_MAPPING.index(direction)
-    except ValueError as err:
-        err.args = (f'"{direction}" is not a value accepted in the current mapping : {RAW_DIR_TO_LABEL_MAPPING}',)
-        raise
 
 
 def _five_direction_joystick_mapping(joystick):
