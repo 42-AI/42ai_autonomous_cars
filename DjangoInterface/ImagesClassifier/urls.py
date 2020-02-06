1128177cddac937fb1df04d65a4d63dc78aa13d3
eparisot <eparisot@student.42.fr>
2020-02-06 15:22:19 +0100

    added switch and textarea (label preview)

--- DjangoInterface/ImagesClassifier/urls.py
+++ DjangoInterface/ImagesClassifier/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('main.urls')),
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
 ]
 
 urlpatterns += [
