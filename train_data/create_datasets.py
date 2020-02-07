4e1c74cbcf3fd401e11b6e540c448de5b232ed09
Pablo <pablo87clement@gmail.com>
2020-02-07 17:03:18 +0100

    skeletton

---
+++ train_data/create_datasets.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import pathlib
+
+# args path to labels.json
+"""
+ouvrir et parser json pr avoir dico
+--> get_dir of labels.json + list of images
+dans dataset features 
+dans dataset labels tensor(direction, speed)
+
+map pour avoir image 
+
+fonction pour montrer au fur et a mesure
+
+"""
