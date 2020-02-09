57a6e04310257bf9c03101527b1ec523f68c84f6
jjauzion <wants-to-fly@hotmail.fr>
2020-02-09 18:08:09 +0100

    added created_on for dataset to index template + minor fix create_index.py

--- get_data/create_index.py
+++ get_data/create_index.py
@@ -14,7 +14,7 @@ from conf.cluster_conf import ES_HOST_IP, ES_HOST_PORT, ES_INDEX
 def get_args(description):
     parser = argparse.ArgumentParser(description=description,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("-i", "--index", type=str,
+    parser.add_argument("-i", "--index", type=str, default=f'{ES_INDEX}-{datetime.now().strftime("%y%m%d")}',
                         help=f'Name of the new index. By default : "{ES_INDEX}-today_date"')
     return parser.parse_args()
 
@@ -22,16 +22,15 @@ def get_args(description):
 def create_index():
     """Create a new index and set alias to the index defined in cluster_conf file and set the
     new index as the write index for this alias."""
-    CURRENT_WRITE_INDEX = "patate-db-200205"
+    CURRENT_WRITE_INDEX = "patate-db-200206"
     args = get_args(create_index.__doc__)
-    index_name = f'{ES_INDEX}-{datetime.now().strftime("%y%m%d")}'
     es_utils.create_es_index(host_ip=ES_HOST_IP,
                              host_port=ES_HOST_PORT,
-                             index_name=index_name,
+                             index_name=args.index,
                              alias=ES_INDEX,
                              is_write_index=True,
                              current_write_index=CURRENT_WRITE_INDEX)
-    print(f'Index "{index_name}" created and defined as the new write index for alias "{ES_INDEX}"')
+    print(f'Index "{args.index}" created and defined as the new write index for alias "{ES_INDEX}"')
 
 
 if __name__ == "__main__":
