f7ed23c742854ca873c44512f7d7b97d3cd69c1f
Pablo <pablo87clement@gmail.com>
2020-02-28 13:55:59 +0100

    Change race.py dir/speed indices

--- race.py
+++ race.py
@@ -70,16 +70,16 @@ class RaceOn:
         print("RaceOn initialized")
 
     def _get_motor_direction(self, predicted_labels):
-        return self.car_mapping.get_raw_dir_from_label(predicted_labels[1])
+        return self.car_mapping.get_raw_dir_from_label(predicted_labels[0])
 
     def _get_motor_speed(self, predicted_labels):
-        if predicted_labels[1] == self.dir_center_label and predicted_labels[0] > 0:
-            return self.car_mapping.get_raw_speed_from_label(predicted_labels[0])
+        if predicted_labels[0] == self.dir_center_label and predicted_labels[1] > 0:
+            return self.car_mapping.get_raw_speed_from_label(predicted_labels[1])
         return self.car_mapping.get_raw_speed_from_label(0)
 
     @staticmethod
     def _get_motor_head(predicted_labels):
-        if predicted_labels[0] > 0:
+        if predicted_labels[1] > 0:
             return HEAD_UP
         return HEAD_DOWN
 
