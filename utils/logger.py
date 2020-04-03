d923683264d1406cf756f064bf458a3dbbe93e74
jjauzion <jeremy.jauzion@gmail.com>
2020-04-03 12:06:02 +0200

    removing useless line

--- utils/logger.py
+++ utils/logger.py
@@ -72,7 +72,6 @@ class Logger:
 
 
 if __name__ == "__main__":
-    # logging.basicConfig(filename="file.log", format='%(asctime)s %(message)s', )
     logger = Logger().create(logger_name=__name__)
 
     # 'application' code
